11877cf62332bde7c2fbafeb54ad0c7b24e77d8e
abdalla <abdallah.mahmooud@reno.sytems>
2024-12-19 16:03:26 +0200

    configuring google and github for sign-in and signup , getting the secrets and ids

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,6 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "./lib/db";
 import authConfig from "./auth.config";
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  ...authConfig,
   adapter: PrismaAdapter(db),
+  ...authConfig,
 });
