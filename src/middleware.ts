0fffb3fd7c815318548e304b736f682d757c8563
abdalla <abdallah.mahmooud@reno.sytems>
2024-12-20 07:10:45 +0200

    handling all the routes in the middleware , redirecting the unauthenticated users to the authentication pages , and redirecting authenticated users to the home page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,28 @@
 import { auth } from "./auth";
+import { apiAuthRoute, authneticationRoutes } from "./routes";
 
 export default auth(async (request) => {
   console.log(request);
-  // const { nextUrl } = request;
-  // console.log(request.auth);
-  // const loggedIn = !!request.auth;
-  // console.log(loggedIn);
+  const { nextUrl } = request;
+  const loggedIn = !!request.auth;
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthRoute);
+  const isAuthenticationRoute = authneticationRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return;
+  }
+
+  if (loggedIn) {
+    if (isAuthenticationRoute) {
+      return Response.redirect(new URL("/", nextUrl));
+    }
+    return;
+  }
+  if (!loggedIn && isAuthenticationRoute) {
+    return;
+  }
+  return Response.redirect(new URL(authneticationRoutes[0], nextUrl));
+
   // if (nextUrl.pathname.startsWith("/api/auth")) {
   //   return;
   // }
---
+++ src/routes.ts
@@ -0,0 +1,3 @@
+export const apiAuthRoute: string = "/api/auth";
+
+export const authneticationRoutes: string[] = ["/auth/signin", "/auth/signup"];
