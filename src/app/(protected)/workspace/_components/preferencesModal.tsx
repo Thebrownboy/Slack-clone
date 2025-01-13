9dd4d5cb1d6a9a28c9ef84c494cab7c53180191b
abdalla <abdallah.mahmooud@reno.sytems>
2025-01-13 23:33:47 +0200

    start implementing the preferences modal

---
+++ src/app/(protected)/workspace/_components/preferencesModal.tsx
@@ -0,0 +1,45 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import React, { useEffect, useState } from "react";
+
+interface PrefernceModalProps {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  initialValue: string;
+}
+
+export default function PreferencesModal({
+  initialValue,
+  open,
+  setOpen,
+}: PrefernceModalProps) {
+  const [value, setValue] = useState(initialValue);
+
+  useEffect(() => {
+    setValue(initialValue);
+  }, [initialValue]);
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent className="p-0 bg-gray-50 overflow-hidden">
+        <DialogHeader className="p-4 border-b bg-white">
+          <DialogTitle>{value}</DialogTitle>
+        </DialogHeader>
+        <div className="px-4 pb-4 flex flex-col gap-y-2">
+          <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
+            <div className="flex items-center justify-between">
+              <p className="text-sm font-semibold">Workspace name</p>
+              <p className=" text-sm text-[#1264a3] hover:underline font-semibold">
+                Edit
+              </p>
+            </div>
+            <p className="text-sm">{value}</p>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/state-store/store.ts
+++ src/state-store/store.ts
@@ -1,3 +1,4 @@
+import { tWorkspace } from "@/types/common-types";
 import { create } from "zustand";
 
 interface iCreateWorkspaceModal {
@@ -5,11 +6,31 @@ interface iCreateWorkspaceModal {
   setOpen(open: boolean): void;
 }
 
+interface iCurrentWorkspace {
+  isLoading: boolean;
+  updateLoading(loading: boolean): void;
+  workSpace: tWorkspace | null;
+  updateWorkspace(workspace: tWorkspace | null): void;
+}
+
 export const useWorkSpaceStore = create<iCreateWorkspaceModal>((set) => {
   return {
     isOpen: false,
     setOpen(open: boolean) {
       set({ isOpen: open });
+    },
+  };
+});
+
+export const useCurrentWorkspace = create<iCurrentWorkspace>((set) => {
+  return {
+    isLoading: true,
+    updateLoading(loading: boolean) {
+      set((state) => ({ ...state, isLoading: loading }));
+    },
+    workSpace: null,
+    updateWorkspace(workspace: tWorkspace | null) {
+      set((state) => ({ ...state, workSpace: workspace }));
     },
   };
 });
