e2da4cf24716c00f83fbd753bc73b0cdff444b12
abdalla <abdallah.mahmooud@reno.sytems>
2025-01-18 01:43:40 +0200

    unifiying the values of the current workspace state in one object instead

--- src/state-store/store.ts
+++ src/state-store/store.ts
@@ -1,6 +1,46 @@
 import { tWorkspace } from "@/types/common-types";
 import { create } from "zustand";
 
+interface iCreateWorkspaceModal {
+  isOpen: boolean;
+  setOpen(open: boolean): void;
+}
+
+export const useCreateWorkspaceModal = create<iCreateWorkspaceModal>((set) => {
+  return {
+    isOpen: false,
+    setOpen(open: boolean) {
+      set({ isOpen: open });
+    },
+  };
+});
+
+interface iWorkspaceState {
+  isLoading: boolean;
+  workSpace: tWorkspace | null;
+}
+interface iCurrentWorkspace {
+  currentWorkspaceState: iWorkspaceState;
+  updateWorkspaceState(workspaceState: iWorkspaceState): void;
+}
+
+export const useCurrentWorkspace = create<iCurrentWorkspace>((set) => {
+  return {
+    currentWorkspaceState: {
+      isLoading: true,
+      workSpace: null,
+    },
+    updateWorkspaceState(workspaceState: iWorkspaceState) {
+      set((state) => ({
+        currentWorkspaceState: {
+          ...state.currentWorkspaceState,
+          ...workspaceState,
+        },
+      }));
+    },
+  };
+});
+
 interface iUser {
   email: string;
   id: string;
@@ -16,39 +56,6 @@ interface IUserStore {
   userState: iUserState;
   updateState(userState: iUserState): void;
 }
-interface iCreateWorkspaceModal {
-  isOpen: boolean;
-  setOpen(open: boolean): void;
-}
-
-interface iCurrentWorkspace {
-  isLoading: boolean;
-  updateLoading(loading: boolean): void;
-  workSpace: tWorkspace | null;
-  updateWorkspace(workspace: tWorkspace | null): void;
-}
-
-export const useCreateWorkspaceModal = create<iCreateWorkspaceModal>((set) => {
-  return {
-    isOpen: false,
-    setOpen(open: boolean) {
-      set({ isOpen: open });
-    },
-  };
-});
-
-export const useCurrentWorkspace = create<iCurrentWorkspace>((set) => {
-  return {
-    isLoading: true,
-    updateLoading(loading: boolean) {
-      set((state) => ({ ...state, isLoading: loading }));
-    },
-    workSpace: null,
-    updateWorkspace(workspace: tWorkspace | null) {
-      set((state) => ({ ...state, workSpace: workspace }));
-    },
-  };
-});
 
 // why I did not separet the state  and used one object
 // because if you use multiple values and multiple udpate funciton to that values
@@ -63,19 +70,6 @@ export const useCurrentUser = create<IUserStore>((set) => {
     },
     updateState(userState) {
       set((state) => ({ userState: { ...state.userState, ...userState } }));
-    },
-  };
-});
-
-interface Icounter {
-  counter: number;
-  updateCounter(couter: number): void;
-}
-export const useCurrentCounter = create<Icounter>((set) => {
-  return {
-    counter: 0,
-    updateCounter(counter: number) {
-      set((state) => ({ ...state, counter: counter }));
     },
   };
 });
