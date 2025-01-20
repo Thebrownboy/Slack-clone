03e273b42591448ce247dcb50dfffdcc93b54d1c
abdalla <abdallah.mahmooud@reno.sytems>
2025-01-20 23:10:34 +0200

    creating the sidebar items of the workspace sidebar

--- src/app/(protected)/workspace/_components/workspaceSidebar.tsx
+++ src/app/(protected)/workspace/_components/workspaceSidebar.tsx
@@ -1,7 +1,8 @@
 import useGetCurrentMember from "@/features/members/hooks/useGetCurrentMember";
-import { Loader } from "lucide-react";
+import { Loader, MessageSquareText, SendHorizonal } from "lucide-react";
 import WorkspaceHeader from "./workspaceHeader";
 import { useCurrentUser, useCurrentWorkspace } from "@/state-store/store";
+import SidebarItem from "./sidebarItem";
 
 export default function WorkspaceSidebar() {
   const {
@@ -27,6 +28,21 @@ export default function WorkspaceSidebar() {
   return (
     <div className="flex flex-col bg-[#5E2C5F] h-full">
       <WorkspaceHeader isAdmin={member?.role === "admin"} />
+
+      <div className="flex flex-col px-2 mt-3">
+        <SidebarItem
+          label="Threads"
+          icon={MessageSquareText}
+          id="threads"
+          variant={"active"}
+        />
+        <SidebarItem
+          label="Drafts & Sent"
+          icon={SendHorizonal}
+          id="draft"
+          variant={"active"}
+        />
+      </div>
     </div>
   );
 }
