2af332512b33032adbfaa697fa77331c5dcbf683
abdalla <abdallah.mahmooud@reno.sytems>
2025-02-18 02:13:33 +0200

    system under updating

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,6 +1,12 @@
-import SignInCard from "@/features/auth/components/signInCard";
+// import SignInCard from "@/features/auth/components/signInCard";
 import React from "react";
 
 export default function SignIn() {
-  return <SignInCard />;
+  // return <SignInCard />;
+  return (
+    <div style={{ background: "yellow", padding: "10px", textAlign: "center" }}>
+      ⚠️ We are currently updating our system. Some features may be unavailable.
+      ⚠️
+    </div>
+  );
 }
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,6 +1,12 @@
-import SignUpCard from "@/features/auth/components/signUpCard";
+// import SignUpCard from "@/features/auth/components/signUpCard";
 import React from "react";
 
 export default function SignUp() {
-  return <SignUpCard />;
+  // return <SignUpCard />;
+  return (
+    <div style={{ background: "yellow", padding: "10px", textAlign: "center" }}>
+      ⚠️ We are currently updating our system. Some features may be unavailable.
+      ⚠️
+    </div>
+  );
 }
